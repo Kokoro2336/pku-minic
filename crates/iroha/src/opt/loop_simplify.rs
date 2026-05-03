7b608e9ad13d23a275d33cf400c5590b9ade6d26
Kokoro2336 <2529677678@qq.com>
2026-05-03 14:44:58 +0800

    fix(loop-simplify): exits_blocks updating.

--- crates/iroha/src/opt/loop_simplify.rs
+++ crates/iroha/src/opt/loop_simplify.rs
@@ -7,6 +7,7 @@ use yachiyo::base::Type;
 use yachiyo::ir::mid::{Builder, Function, Op, OpData, OpType, Operand, PhiIncoming, IR};
 use yachiyo::pass::Pass;
 use yachiyo::utils::r#match::match_some;
+use yachiyo::utils::set::BitSet;
 
 #[derive(Default)]
 pub struct LoopSimplify<'a> {
@@ -212,8 +213,11 @@ impl LoopSimplify<'_> {
   }
 
   /// For simplicity, we insert one dedicated exiting block.
-  fn dedicated_exits(&mut self, loop_data: &LoopData) {
+  fn dedicated_exits(&mut self, loop_data: &mut LoopData) {
     let func_id = self.builder.current_function.unwrap();
+    let mut deprecated_exits = BitSet::new();
+    let mut new_exits = BitSet::new();
+
     // Find out all exit blocks of the loop, and for each exit block.
     for exit_bb_id in loop_data.exit_blocks.iter() {
       let exit_bb_id = Operand::BB(exit_bb_id);
@@ -221,102 +225,115 @@ impl LoopSimplify<'_> {
       let should_insert_dedicated = exit_bb_preds
         .iter()
         .any(|(pred_id, _)| !loop_data.blocks.contains(pred_id.get_bb_id()));
-      if should_insert_dedicated {
-        for (exit_bb_pred_id, _) in exit_bb_preds.iter() {
-          if loop_data.blocks.contains(exit_bb_pred_id.get_bb_id()) {
-            // If the exit block has preds from the loop, insert a dedicated exit block.
-            let new_exit_bb_id = self
-              .builder
-              .create_new_block(self.ir.as_mut().unwrap(), Some(func_id));
-            self.builder.set_current_block(new_exit_bb_id);
-            // Create jump to exit_bb_id
-            self.create(Op::new(
-              Type::Void,
-              vec![],
-              OpData::Jump {
-                target_bb: exit_bb_id,
-              },
-            ));
+      if !should_insert_dedicated {
+        continue;
+      }
 
-            // Replace terminator of exit_bb_pred_id
-            let pred = &self.get_func(func_id).cfg[*exit_bb_pred_id];
-            let term_id = *pred.cur.last().unwrap();
-            let term_op_data = self.get_func(func_id).dfg[term_id].data.clone();
-            match_some! {
-              target: term_op_data,
-              enu: OpData,
-              minor_arms: {
-                OpData::Jump { target_bb } => {
-                  if target_bb == exit_bb_id {
-                    self.replace_op(term_id, *exit_bb_pred_id, Op::new(
-                      Type::Void,
-                      vec![],
-                      OpData::Jump {
-                        target_bb: new_exit_bb_id,
-                      },
-                    ));
-                  } else {
-                    panic!("LoopSimplify: jump terminator does not target the loop exit");
-                  }
+      // Set as deprecated.
+      deprecated_exits.insert(exit_bb_id.get_bb_id());
+
+      for (exit_bb_pred_id, _) in exit_bb_preds.iter() {
+        if loop_data.blocks.contains(exit_bb_pred_id.get_bb_id()) {
+          // If the exit block has preds from the loop, insert a dedicated exit block.
+          let new_exit_bb_id = self
+            .builder
+            .create_new_block(self.ir.as_mut().unwrap(), Some(func_id));
+          self.builder.set_current_block(new_exit_bb_id);
+
+          // Mark the new exit as the new exit block.
+          new_exits.insert(new_exit_bb_id.get_bb_id());
+
+          // Create jump to exit_bb_id
+          self.create(Op::new(
+            Type::Void,
+            vec![],
+            OpData::Jump {
+              target_bb: exit_bb_id,
+            },
+          ));
+
+          // Replace terminator of exit_bb_pred_id
+          let pred = &self.get_func(func_id).cfg[*exit_bb_pred_id];
+          let term_id = *pred.cur.last().unwrap();
+          let term_op_data = self.get_func(func_id).dfg[term_id].data.clone();
+          match_some! {
+            target: term_op_data,
+            enu: OpData,
+            minor_arms: {
+              OpData::Jump { target_bb } => {
+                if target_bb == exit_bb_id {
+                  self.replace_op(term_id, *exit_bb_pred_id, Op::new(
+                    Type::Void,
+                    vec![],
+                    OpData::Jump {
+                      target_bb: new_exit_bb_id,
+                    },
+                  ));
+                } else {
+                  panic!("LoopSimplify: jump terminator does not target the loop exit");
                 }
-                OpData::Br { cond, then_bb, else_bb } => {
-                  if then_bb == exit_bb_id {
-                    self.replace_op(term_id, *exit_bb_pred_id, Op::new(
-                      Type::Void,
-                      vec![],
-                      OpData::Br {
-                        cond,
-                        then_bb: new_exit_bb_id,
-                        else_bb,
-                      },
-                    ));
-                  } else if else_bb == exit_bb_id {
-                    self.replace_op(term_id, *exit_bb_pred_id, Op::new(
-                      Type::Void,
-                      vec![],
-                      OpData::Br {
-                        cond,
-                        then_bb,
-                        else_bb: new_exit_bb_id,
-                      },
-                    ));
-                  } else {
-                    panic!("LoopSimplify: branch terminator does not target the loop exit");
-                  }
-                }
-              },
-              uni_ops: [GlobalAlloca, Alloca, Load, Store, Call, Ret, AddI, SubI, MulI, DivI, ModI, AddF, SubF, MulF, DivF, Phi, GEP, Declare, Xor, Shl, Sar, Shr, Sitofp, Fptosi, Uitofp, Zext, SNe, SEq, SLe, SLt, SGe, SGt, OEq, OLt, OLe, ONe, OGe, OGt],
-              uni_arm: {
-                unreachable!("Unexpected terminator op: {:?}", term_op_data);
               }
+              OpData::Br { cond, then_bb, else_bb } => {
+                if then_bb == exit_bb_id {
+                  self.replace_op(term_id, *exit_bb_pred_id, Op::new(
+                    Type::Void,
+                    vec![],
+                    OpData::Br {
+                      cond,
+                      then_bb: new_exit_bb_id,
+                      else_bb,
+                    },
+                  ));
+                } else if else_bb == exit_bb_id {
+                  self.replace_op(term_id, *exit_bb_pred_id, Op::new(
+                    Type::Void,
+                    vec![],
+                    OpData::Br {
+                      cond,
+                      then_bb,
+                      else_bb: new_exit_bb_id,
+                    },
+                  ));
+                } else {
+                  panic!("LoopSimplify: branch terminator does not target the loop exit");
+                }
+              }
+            },
+            uni_ops: [GlobalAlloca, Alloca, Load, Store, Call, Ret, AddI, SubI, MulI, DivI, ModI, AddF, SubF, MulF, DivF, Phi, GEP, Declare, Xor, Shl, Sar, Shr, Sitofp, Fptosi, Uitofp, Zext, SNe, SEq, SLe, SLt, SGe, SGt, OEq, OLt, OLe, ONe, OGe, OGt],
+            uni_arm: {
+              unreachable!("Unexpected terminator op: {:?}", term_op_data);
             }
+          }
 
-            // Process phi nodes in the exit block.
-            let phis = self.get_all_ops_in_block(exit_bb_id, OpType::Phi);
-            for phi_id in phis {
-              let phi_op_data = self.get_func(func_id).dfg[phi_id].data.clone();
-              if let OpData::Phi { incomings } = phi_op_data {
-                for incoming in incomings {
-                  if let PhiIncoming::Data { value, bb } = incoming {
-                    if bb == *exit_bb_pred_id {
-                      self.slay_phi_incoming(phi_id, bb);
-                      self.append_phi_incoming(phi_id, new_exit_bb_id, value);
-                    }
-                  } else {
-                    unreachable!()
+          // Process phi nodes in the exit block.
+          let phis = self.get_all_ops_in_block(exit_bb_id, OpType::Phi);
+          for phi_id in phis {
+            let phi_op_data = self.get_func(func_id).dfg[phi_id].data.clone();
+            if let OpData::Phi { incomings } = phi_op_data {
+              for incoming in incomings {
+                if let PhiIncoming::Data { value, bb } = incoming {
+                  if bb == *exit_bb_pred_id {
+                    self.slay_phi_incoming(phi_id, bb);
+                    self.append_phi_incoming(phi_id, new_exit_bb_id, value);
                   }
+                } else {
+                  unreachable!()
                 }
-              } else {
-                unreachable!()
               }
+            } else {
+              unreachable!()
             }
           }
         }
       }
     }
+
+    // Update exit_blocks.
+    loop_data.exit_blocks &= !deprecated_exits;
+    loop_data.exit_blocks |= new_exits;
   }
 
-  fn run(&mut self, dom_tree: &DomTree, loops_data: &Vec<LoopData>) {
+  fn run(&mut self, dom_tree: &DomTree, loops_data: &mut Vec<LoopData>) {
     let func_id = self.builder.current_function.unwrap();
     for loop_data in loops_data {
       // Ensure dedicated exits first.
@@ -351,9 +368,9 @@ impl<'a> Pass<'a> for LoopSimplify<'a> {
       let func_id = Operand::Func(func_id);
       self.init(func_id);
       let func = self.get_func(func_id);
-      let (loops_data, _) = analyze::<LoopAnalysis>(func);
+      let (mut loops_data, _) = analyze::<LoopAnalysis>(func);
       let (dom_tree, _) = analyze::<DomAnalysis>(func);
-      self.run(&dom_tree, &loops_data);
+      self.run(&dom_tree, &mut loops_data);
     }
   }
 }
